fd6cb322a85899b6def7f708722e7f733b2707da
Niraj Nepal <niraj.nepal70@gmail.com>
2023-08-23 21:32:39 -0400

    Updated styling for welcome section, updated home code

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import './Home.css';
-import NavBar from './NavBar/NavBar';
-
-
+import React from "react";
+import "./Home.css"; // Make sure to import your CSS file
 
 function Home() {
-    return (
-        <div className="section-1">
-            <div className="section-1-text">
-                
-                
-                </div>
+  return (
+    <div className="home">
+      <div className="section-1-left">
+        <div className="section-1-text">
+        
+          <h>Hello! I'm Niraj Nepal</h>
+          <h1 className="title">Software Engineer</h1>
+          <p>
+            I am a recent graduate with  a major in Computer Science<br/>
+             and minors in Cyber Security and Mathematics.I am an
+             <br/>Enthusiastic individual ready for what's ahead!
+          </p>
+        </div>
+      </div>
+      <div className="section-1-right">
+        <div className="rounded-image">
+          <img src="https://scontent.fagc3-1.fna.fbcdn.net/v/t39.30808-6/349061242_154854020900804_2183159603774144043_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=V-3N3IuoPuEAX8Lw5WY&_nc_ht=scontent.fagc3-1.fna&oh=00_AfBxcNihhHyZpKgVkgF2QMG3ssaSID5y0mgtOP30HdJdGA&oe=64EB3D1F" alt="Niraj" />
+        </div>
+      </div>
+
+        <div className="section-2">
+            
         </div>
 
-    )
+
+
+
+
+
+
+    </div>
+
+    
+  );
 }
 
 export default Home;
--- src/views/Home/NavBar/NavBar.js
+++ src/views/Home/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./NavBar.css";
+import Nlogo from "./Nlogo.png";
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <div> <img className="card__image" src="https://scontent.fagc3-1.fna.fbcdn.net/v/t39.30808-6/349061242_154854020900804_2183159603774144043_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=V-3N3IuoPuEAX8Lw5WY&_nc_ht=scontent.fagc3-1.fna&oh=00_AfBxcNihhHyZpKgVkgF2QMG3ssaSID5y0mgtOP30HdJdGA&oe=64EB3D1F" alt="Picture of Niraj Nepal"/> </div>
+      <div> <img src="Nlogo.png"/></div>  
 
 
       <ul className="navbar-links">
